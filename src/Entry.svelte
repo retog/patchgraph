38742315286ac4b1fc333f9f18b6963d0623407c
Reto Gmür <reto@factsmission.com>
2020-11-01 17:38:39 +0100

    No Mardown!

--- src/Entry.svelte
+++ src/Entry.svelte
@@ -1,39 +1,10 @@
 <script>
-  import * as Factory from "@rdfjs/data-model";
-  import Dataset from "@rdfjs/dataset";
-  import * as N3 from "n3";
   import {link} from 'svelte-spa-router'
-  import {
-    DatasetBrowser,
-  } from "rdfjs-svelte";
 
   global.setImmediate = function(f) {
     setTimeout(f,0);
   }
-  let dataset = Dataset.dataset([]);
-  const parser = new N3.Parser();
 
-  fetch("./tbbt.nt")
-    .then((r) => r.text())
-    .then((t) => {
-      parser.parse(
-        t,
-        (error, quad, prefixes) => {
-          if (quad) {
-            console.log(JSON.stringify(quad));
-            dataset.add(quad);
-          }
-          else {
-            console.log("# Tjhjhl, folks!", prefixes);
-            console.log("dataset", dataset);
-            dataset = dataset;
-          }
-        }
-      );
-    });
-
-
-  $: console.log("dataset", dataset);
 </script>
 
 <style>
@@ -64,10 +35,10 @@
   <h1>MyQuads</h1>
 
   <p>Store triples and quads in your browser and use SPARQL to retreive them.</p>
+  <p>MyQuads works thanks to the following projects:</p>
   <ul>
-    <li>[Quadstore](https://github.com/beautifulinteractions/node-quadstore)
-    </li>
-    <li>[Yasgui](https://github.com/TriplyDB/Yasgui)</li>
-    <li>[rdfjs-svelte](https://github.com/linked-solutions/rdfjs-svelte)</li>
+    <li><a href="https://github.com/beautifulinteractions/node-quadstore">Quadstore</a></li>
+    <li><a href="https://github.com/TriplyDB/Yasgui">Yasgui</a></li>
+    <li><a href="https://github.com/linked-solutions/rdfjs-svelte">rdfjs-svelte</a></li>
   </ul>
 </main>
