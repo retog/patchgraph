6f2ade2be723d87cc9fcf48004ce9ef8c7a0f0db
Reto Gmür <reto@factsmission.com>
2020-11-01 22:26:15 +0100

    typo

--- src/Entry.svelte
+++ src/Entry.svelte
@@ -9,10 +9,10 @@
 <main>
   <h1>MyQuads</h1>
 
-  <p>Store triples and quads in your browser and use SPARQL to retreive them.</p>
+  <p>Store triples and quads in your browser and use SPARQL to retrieve them.</p>
   <p>MyQuads works thanks to the following projects:</p>
   <ul>
-    <li><a href="https://github.com/beautifulinteractions/node-quadstore">Quadstore</a></li>
+    <li><a href="https://github.com/beautifulinteractions/node-quadstore">Beautiful Interactions' Quadstore</a></li>
     <li><a href="https://github.com/TriplyDB/Yasgui">Yasgui</a></li>
     <li><a href="https://github.com/linked-solutions/rdfjs-svelte">rdfjs-svelte</a></li>
   </ul>
