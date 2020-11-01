31faa46e54da02ba501ded86a521bfe4b1e5756b
Reto Gmür <reto@factsmission.com>
2020-11-01 18:36:53 +0100

    nicer menu

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,28 @@
   import Router from 'svelte-spa-router'
   import { routes } from './routes'
   import { link } from 'svelte-spa-router'
+  import active from 'svelte-spa-router/active'
 
   document.title = 'MyQuads'
 </script>
-<ul>
-  <li><a href="/" use:link>Entry page</a></li>
-  <li><a href="/sparql" use:link>Sparql</a></li>
-  <li><a href="/all-quads" use:link>All quads</a></li>
+
+<style>
+  ul#menu li {
+    display:inline;
+  }
+
+  ul#menu li:not(:first-child) ::before {
+    content:' | ';
+  }
+  :global(a.active) {
+      color: red;
+  }
+</style>
+
+<ul id="menu">
+  <li><a href="/" use:link use:active>Entry page</a></li>
+  <li><a href="/sparql" use:link use:active>Sparql</a></li>
+  <li><a href="/all-quads" use:link use:active>All quads</a></li>
 </ul>
 
 <Router {routes} />
