edf033e2b7769e24619bdad2edc17d4b693b7ad7
Reto Gmür <reto@factsmission.com>
2020-11-01 18:50:21 +0100

    formating

--- src/AllQuads.svelte
+++ src/AllQuads.svelte
@@ -2,30 +2,26 @@
   import * as Factory from "@rdfjs/data-model";
   import Dataset from "@rdfjs/dataset";
   import { DatasetEditor } from "rdfjs-svelte";
-  import {Quadstore} from 'quadstore'
-  import leveljs from 'level-js';
+  import { Quadstore } from "quadstore";
+  import leveljs from "level-js";
 
   let dataset = Dataset.dataset([]);
 
-
-    const load = async function() {
-
-     const store = new Quadstore({
-        dataFactory: Factory,
-        backend: leveljs('quadstore'),
-      });
-      await store.open();
-      //const pattern = {graph: Factory.namedNode('http://ex.com/g')};
-      const { items } = await store.get({});
-      dataset = Dataset.dataset(items)
-    }
-    load().catch(e => console.log(e))
+  const load = async function () {
+    const store = new Quadstore({
+      dataFactory: Factory,
+      backend: leveljs("quadstore"),
+    });
+    await store.open();
+    //const pattern = {graph: Factory.namedNode('http://ex.com/g')};
+    const { items } = await store.get({});
+    dataset = Dataset.dataset(items);
+  };
+  load().catch((e) => console.log(e));
   $: console.log("dataset", dataset);
 </script>
 
 <main>
-
   <h1>Dataset</h1>
   <DatasetEditor bind:value={dataset} />
-
 </main>
